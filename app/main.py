2ca579726a6113b6745425bc5cf51c446786863e
DiegoAvellaneda <48260188+diegoavellanedat17@users.noreply.github.com>
2024-07-08 23:27:08 -0500

    cors to connect the BE and FE

--- app/main.py
+++ app/main.py
@@ -1,8 +1,17 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from .database.init_db import init_db
 from .routers import users, tasks
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Allow all origins, adjust this for production
+    allow_credentials=True,
+    allow_methods=["*"],  # Allow all methods (GET, POST, PUT, DELETE, etc.)
+    allow_headers=["*"],  # Allow all headers
+)
 
 init_db()
 
